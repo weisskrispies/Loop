8e43ed27196a74935543a3b61dedf630f36dd7f7
weisskrispies <patrick.weiss@gmail.com>
2015-10-20 07:23:43 -0700

    Initial Commit

---
+++ Loop/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Loop
+//
+//  Created by Patrick Weiss on 10/20/15.
+//  Copyright © 2015 Patrick Weiss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
